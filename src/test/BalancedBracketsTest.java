082298b34fc64d093a0dd6307795d707fef70529
masontrang <mt9260@gmail.com>
2022-01-24 21:30:08 -0500

    commit

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,70 @@ import static org.junit.Assert.*;
 public class BalancedBracketsTest {
 
     //TODO: add tests here
+
     @Test
-    public void emptyTest() {
-        assertEquals(true, true);
+    public void onlyBracketsReturnsTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
     }
+
+    @Test
+    public void balancedBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[test]"));
+    }
+
+    @Test
+    public void brackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test[]"));
+    }
+
+    @Test
+    public void bracketsReturnTrue() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("test[test]"));
+    }
+
+    @Test
+    public void noBrackets() {
+        assertTrue(BalancedBrackets.hasBalancedBrackets("nobrackets"));
+    }
+
+    @Test
+    public void opposingBrackets() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void extraBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[]]"));
+    }
+
+    @Test
+    public void singleLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[singlebracket"));
+    }
+
+    @Test
+    public void singleRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("singlebracket]"));
+    }
+
+    @Test
+    public void doubleLeftBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[[singlebracket]"));
+    }
+
+    @Test
+    public void doubleRightBracket() {
+        assertFalse(BalancedBrackets.hasBalancedBrackets("[singlebracket]]"));
+    }
+
+    @Test
+    public void notNull() {
+        assertNotNull(BalancedBrackets.hasBalancedBrackets(""));
+    }
+
+
+
+
 
 
 }
